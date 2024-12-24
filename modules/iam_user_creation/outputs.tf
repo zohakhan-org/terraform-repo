9bb9f87e5399b66cbc060ae76c3dae10ed42aa9d
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-24 13:51:18 -0700

    Initial commit

--- modules/iam_user_creation/outputs.tf
+++ modules/iam_user_creation/outputs.tf
@@ -2,3 +2,7 @@ output "role_arn" {
   description = "The arn of the user"
   value = aws_iam_user.iam_user.arn
 }
+
+output "debug_user_name" {
+  value = aws_iam_user.iam_user.name
+}
--- scripts/terraform_deploy.sh
+++ scripts/terraform_deploy.sh
@@ -90,6 +90,7 @@ for SERVICE in $SELECTED_SERVICES; do
       terraform -chdir="$MODULE_PATH" validate
       echo "terraform plan"
       ls -lrt "$MODULE_PATH"
+      terraform -chdir="$MODULE_PATH" refresh -var-file="$TFVARS_FILE"
 
       terraform -chdir="$MODULE_PATH" plan  -var-file="$TFVARS_FILE"  -target=modules.iam_user_creation
       echo "Terraform apply"
