fd63e3a3d8a59a7d817297dd0f4ccf14a23ef6e7
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-26 15:53:25 -0700

    Initial commit

--- modules/ec2/backend.tf
+++ modules/ec2/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket="tf-state-25tfs"
     key="development/terraform_state"
     region="us-east-1"
-    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
