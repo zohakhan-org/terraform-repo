cf2a4f0d95fb9f0702f727f1a65c41dae9ae8f46
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-26 10:05:09 -0700

    Initial commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,26 @@
-resource "aws_instance" "ec2_instance" {
-  ami = var.ec2_ami_id
-  instance_type = var.ec2_instance_type
-    count = var.ec2_instance_count
+data "aws_availability_zones" "available" {}
+data "aws_ami" "latest-ubuntu" {
+    most_recent = true
+    owners = ["099720109477"]
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
 
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami = data.aws_ami.latest-ubuntu.id
+  instance_type = var.ec2_instance_type
+  count = var.ec2_instance_count
+  availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    Name = var.ec2_instance_name
+    Name = (var.ec2_instance_name)-count.index
   }
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -22,3 +22,13 @@ variable "ec2_instance_count" {
   description = "Number of instances to create"
     type        = number
 }
+
+variable "AMIS" {
+  type = map
+  default= {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-east-2 = "ami-05803413c51f242b7"
+    us-west-1 = "ami-0454207e5367abf01"
+    us-west-2 = "ami-0688ba7eeeeefe3cd"
+  }
+}
