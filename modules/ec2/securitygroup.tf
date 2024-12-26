37862ebfc0b05b6026b16c8f218270dcff3db15c
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-26 10:33:59 -0700

    Initial commit

---
+++ modules/ec2/securitygroup.tf
@@ -0,0 +1,18 @@
+data "aws_ip_ranges" "us_east_ip_ranges" {
+  region = var.aws_region
+  services =["ec2"]
+}
+resource "aws_security_group" "sg-custom_us_east" {
+  name = "sg-custom-us-east"
+  ingress {
+    from_port = "443"
+    to_port = "443"
+    protocol = "tcp"
+    cidr_blocks = [data.aws_ip_ranges.us_east_ip_ranges.cidr_blocks]
+
+  }
+  tags = {
+    CreateDate= data.aws_ip_ranges.us_east_ip_ranges.create_date
+    SyncToken= data.aws_ip_ranges.us_east_ip_ranges.sync_token
+  }
+}
