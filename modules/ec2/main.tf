f425e2d4f995dcf5b138edaf2c2e31bbd3bbb2fb
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-26 11:10:09 -0700

    Initial commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -19,6 +19,9 @@ resource "aws_instance" "ec2_instance" {
   instance_type = var.ec2_instance_type
   count = var.ec2_instance_count
   availability_zone = data.aws_availability_zones.available.names[1]
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.ec2_instance.private_ip} >> private_ips.txt"
+  }
 
   tags = {
     Name = (var.ec2_instance_name)
