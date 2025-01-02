62065173401422427c2d677d2634a801143f76b0
zohakhanlearning <zohakhanlearning@gmail.com>
2025-01-02 09:19:03 -0700

    Initial commit

---
+++ modules/eks/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_region" "current" {
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
+}
---
+++ modules/eks/variable.tf
@@ -0,0 +1,9 @@
+variable "cluster-name" {
+  default = "pocket-tf-eks"
+  type    = string
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+  type = string
+}
